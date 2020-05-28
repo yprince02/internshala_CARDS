9f541c9fef0755889355a678f3aa469848034a38
Prince Yadav <yprince0219@gmail.com>
2020-05-28 07:30:26 +0530

    beautify syntax

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
-    $(".num").counterUp({delay:10,time:1000});
+ //NUMBER COUNT
+$(".num").counterUp({ delay: 10, time: 1000 });
 
 
+//HEADING AUTOTYPE
 
 {
-	    const heading = 'Meet Our Team';
-let i = 0;
+    const heading = 'Meet Our Team';
+    let i = 0;
 
-const typing = () => {
-  if (i < heading.length) {
-    document.querySelector('.heading').innerHTML += heading.charAt(i);
-    i++;
-    setTimeout(typing, 150);
-  }
-};
+    const typing = () => {
+        if (i < heading.length) {
+            document.querySelector('.heading').innerHTML += heading.charAt(i);
+            i++;
+            setTimeout(typing, 150);
+        }
+    };
 
-typing();
+    typing();
 }
